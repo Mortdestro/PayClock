23e9adf275ed49b3b39afd5adc352e1a1efd9080
Mortdestro <danmalear@gmail.com>
2020-03-22 23:50:11 -0700

    Version 1

---
+++ PayClock/PayClock.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace PayClock
+{
+    partial class PayClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.textBoxRate = new System.Windows.Forms.TextBox();
+            this.labelRate = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelDollar = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.panelRate = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel.SuspendLayout();
+            this.panelRate.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(3, 53);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Location = new System.Drawing.Point(264, 53);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartStop.TabIndex = 3;
+            this.buttonStartStop.Text = "Start";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
+            // 
+            // textBoxRate
+            // 
+            this.textBoxRate.Location = new System.Drawing.Point(16, 16);
+            this.textBoxRate.MaxLength = 8;
+            this.textBoxRate.Name = "textBoxRate";
+            this.textBoxRate.Size = new System.Drawing.Size(62, 20);
+            this.textBoxRate.TabIndex = 0;
+            this.textBoxRate.TextChanged += new System.EventHandler(this.textBoxRate_TextChanged);
+            // 
+            // labelRate
+            // 
+            this.labelRate.AutoSize = true;
+            this.labelRate.Location = new System.Drawing.Point(3, 0);
+            this.labelRate.Name = "labelRate";
+            this.labelRate.Size = new System.Drawing.Size(59, 13);
+            this.labelRate.TabIndex = 3;
+            this.labelRate.Text = "Rate /hour";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(90, 53);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "&Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.tableLayoutPanel.SetColumnSpan(this.labelTotal, 3);
+            this.labelTotal.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.Location = new System.Drawing.Point(229, 0);
+            this.labelTotal.Margin = new System.Windows.Forms.Padding(0);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelTotal.Size = new System.Drawing.Size(119, 50);
+            this.labelTotal.TabIndex = 5;
+            this.labelTotal.Text = "$0.00";
+            // 
+            // labelDollar
+            // 
+            this.labelDollar.AutoSize = true;
+            this.labelDollar.Location = new System.Drawing.Point(3, 19);
+            this.labelDollar.Name = "labelDollar";
+            this.labelDollar.Size = new System.Drawing.Size(13, 13);
+            this.labelDollar.TabIndex = 6;
+            this.labelDollar.Text = "$";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(177, 53);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 7;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 4;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.Controls.Add(this.panelRate, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.labelTotal, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.buttonReset, 2, 1);
+            this.tableLayoutPanel.Controls.Add(this.buttonStartStop, 3, 1);
+            this.tableLayoutPanel.Controls.Add(this.buttonClose, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.buttonSave, 1, 1);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(348, 84);
+            this.tableLayoutPanel.TabIndex = 8;
+            // 
+            // panelRate
+            // 
+            this.panelRate.Controls.Add(this.labelRate);
+            this.panelRate.Controls.Add(this.labelDollar);
+            this.panelRate.Controls.Add(this.textBoxRate);
+            this.panelRate.Location = new System.Drawing.Point(3, 3);
+            this.panelRate.Name = "panelRate";
+            this.panelRate.Size = new System.Drawing.Size(80, 44);
+            this.panelRate.TabIndex = 8;
+            // 
+            // PayClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 85);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(365, 124);
+            this.MinimizeBox = false;
+            this.Name = "PayClock";
+            this.Text = "Pay Clock";
+            this.TopMost = true;
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.panelRate.ResumeLayout(false);
+            this.panelRate.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.TextBox textBoxRate;
+        private System.Windows.Forms.Label labelRate;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelDollar;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Panel panelRate;
+    }
+}
+
---
+++ PayClock/PayClock.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace PayClock
+{
+    public partial class PayClock : Form
+    {
+        private const string SAVE_PATH = "%APPDATA%\\PayClock\\";
+        private const string SAVE_FILE = "data.txt";
+        private const string KACHING_PATH = "kaching.mp3";
+        private const int KACHING_INTERVAL = 10;
+
+        private WindowsMediaPlayer player;
+
+        public Timer Timer { get; set; }
+        public bool Running { get; set; }
+        public double Rate { get; set; }
+        public double Total { get; set; }
+        public double NextKaChing { get; set; }
+
+        public string SavePath { get => Environment.ExpandEnvironmentVariables(SAVE_PATH); }
+        
+        public PayClock()
+        {
+            InitializeComponent();
+            player = new WindowsMediaPlayer
+            {
+                URL = KACHING_PATH
+            };
+
+            Reset();
+            SetTotalText();
+
+            Timer = new Timer();
+            Timer.Tick += new EventHandler(OnTick);
+            Timer.Interval = 50;
+        }
+
+        #region Event handlers
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Save(SavePath, SAVE_FILE, DateTime.Now, Rate, Total);
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if (Running)
+            {
+                Stop();
+            }
+            else
+            {
+                Start();
+            }
+        }
+
+        private void textBoxRate_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBoxRate.Text, out double rate))
+            {
+                buttonStartStop.Enabled = true;
+                Rate = rate;
+            }
+            else
+            {
+                buttonStartStop.Enabled = false;
+            }
+        }
+
+        #endregion
+
+        #region Overrides
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (e.CloseReason == CloseReason.UserClosing)
+                ConfirmClose(e);
+        }
+
+        #endregion
+
+        #region Save file
+
+        private void Save(string savePath, string saveFile, DateTime date, double rate, double total)
+        {
+            // Check for save file and do some processing
+            if (!Directory.Exists(savePath))
+            {
+                Directory.CreateDirectory(savePath);
+            }
+
+            string[] data = ReadFile(savePath + saveFile);
+            StreamWriter sw = new StreamWriter(savePath + saveFile);
+
+            // If it's an empty file, just write this entry
+            bool updated = false;
+            for (int i = 0; i < data.Length; i++)
+            {
+                string line = data[i];
+
+                CheckFormat(line);
+
+                var sections = line.Split(new char[] { ';' });
+                switch (sections[0])
+                {
+                    case "v1":
+                        // version;MM/yyyy;total
+                        string[] dateParts = sections[1].Split('/');
+
+                        if (int.Parse(dateParts[0]) == date.Month && int.Parse(dateParts[1]) == date.Year)
+                        {
+                            line = SaveEntry(date, rate, total);
+                            updated = true;
+                        }
+                        break;
+                    default:
+                        throw new FormatException("Invalid version specified in save file");
+                }
+                sw.WriteLine(line);
+            }
+            if (!updated)
+            {
+                sw.WriteLine(SaveEntry(date, rate, total));
+                updated = true;
+            }
+
+            sw.Close();
+        }
+
+        private string GetSaved(string savePath, string saveFile, DateTime date)
+        {
+            string[] data = ReadFile(savePath + saveFile);
+
+            if (data == null)
+            {
+                return null;
+            }
+            foreach (string line in data)
+            {
+                CheckFormat(line);
+
+                var sections = line.Split(new char[] { ';' });
+                // Check the formatting version
+                switch (sections[0])
+                {
+                    case "v1":
+                        // version;MM/yyyy;total
+                        string[] dateParts = sections[1].Split('/');
+
+                        if (int.Parse(dateParts[0]) == date.Month && int.Parse(dateParts[1]) == date.Year)
+                        {
+                            return line;
+                        }
+                        break;
+                    default:
+                        throw new FormatException("Invalid version specified in save file");
+                }
+            }
+            // If we fall out of the for loop, assume the entry hasn't been added yet
+            return null;
+        }
+
+        private double GetTotal(string entry)
+        {
+            if (string.IsNullOrEmpty(entry))
+            {
+                return 0;
+            }
+            string[] sections = entry.Split(';');
+            // Check formatting version
+            switch (sections[0])
+            {
+                case "v1":
+                    // v1;MM/yyyy;rate;total
+                    return double.Parse(sections[3]);
+                default:
+                    // Shouldn't ever hit here
+                    return 0;
+            }
+        }
+
+        private double GetRate(string entry)
+        {
+            if (string.IsNullOrEmpty(entry))
+            {
+                return 0;
+            }
+            string[] sections = entry.Split(';');
+            // Check formatting version
+            switch (sections[0])
+            {
+                case "v1":
+                    // v1;MM/yyyy;rate;total
+                    return double.Parse(sections[2]);
+                default:
+                    // Shouldn't ever hit here
+                    return 0;
+            }
+        }
+
+        private string[] ReadFile(string path)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                string[] data = sr.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                sr.Close();
+                return data;
+            }
+            catch (Exception e)
+            {
+                if (e is FileNotFoundException || e is DirectoryNotFoundException)
+                {
+                    return null;
+                }
+
+                throw e;
+            }
+        }
+
+        private void CheckFormat(string entry)
+        {
+            var sections = entry.Split(';');
+
+            if (sections.Length <= 0) throw new FormatException("Invalid entry format in save file.");
+
+            string version = sections[0];
+
+            switch (version)
+            {
+                case "v1":
+                    // version;MM/yyyy;total
+                    CheckFormatV1(sections);
+                    break;
+                default:
+                    throw new FormatException("Invalid version specified in save file");
+            }
+        }
+
+        private void CheckFormatV1(string entry)
+        {
+            CheckFormatV1(entry.Split(';'));
+        }
+
+        private void CheckFormatV1(string[] sections)
+        {
+            if (sections.Length != 4) throw new FormatException("Invalid entry format in save file.");
+
+            string[] dateParts = sections[1].Split('/');
+            if (dateParts.Length != 2) throw new FormatException("Invalid date format in save file.");
+
+            if (!int.TryParse(dateParts[0], out int month) || !int.TryParse(dateParts[1], out int year)) throw new FormatException("Invalid date format in save file.");
+            if (!double.TryParse(sections[2], out double rate)) throw new FormatException("Invalid rate format in save file");
+            if (!double.TryParse(sections[3], out double total)) throw new FormatException("Invalid total format in save file");
+        }
+
+        private string SaveEntry(DateTime date, double rate, double total)
+        {
+            return $"v1;{date.Month.ToString("00")}/{date.Year.ToString("0000")};{rate};{total}";
+        }
+
+        #endregion
+
+        #region Timer
+
+        private void Start()
+        {
+            // Disable controls
+            textBoxRate.Enabled = false;
+            buttonClose.Enabled = false;
+            buttonSave.Enabled = false;
+            buttonReset.Enabled = false;
+
+            // Change text
+            buttonStartStop.Text = "Stop";
+
+            Timer.Start();
+            Running = true;
+        }
+
+        private void Stop()
+        {
+            // Enable controls
+            textBoxRate.Enabled = true;
+            buttonClose.Enabled = true;
+            buttonSave.Enabled = true;
+            buttonReset.Enabled = true;
+
+            // Change text
+            buttonStartStop.Text = "Start";
+
+            Timer.Stop();
+            Running = false;
+        }
+
+        private void Reset()
+        {
+            string entry = GetSaved(SavePath, SAVE_FILE, DateTime.Now);
+
+            Rate = GetRate(entry);
+            Total = GetTotal(entry);
+            NextKaChing = (int)Total / KACHING_INTERVAL * KACHING_INTERVAL + KACHING_INTERVAL;
+
+            textBoxRate.Text = Rate.ToString("F");
+            SetTotalText();
+        }
+
+
+        private void OnTick(object obj, EventArgs eventArgs)
+        {
+            Total += Rate * Timer.Interval / 1000 / 60 / 60;
+            SetTotalText();
+            if (Total >= NextKaChing) {
+                player.controls.play();
+                NextKaChing = (int)Total / KACHING_INTERVAL * KACHING_INTERVAL + KACHING_INTERVAL;
+            }
+        }
+        #endregion
+
+        private void ConfirmClose(FormClosingEventArgs e)
+        {
+            switch (MessageBox.Show("Are you sure you want to close the clock?", "Confirm Close", MessageBoxButtons.OKCancel))
+            {
+                case DialogResult.Cancel:
+                    e.Cancel = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SetTotalText()
+        {
+            labelTotal.Text = Total.ToString("C");
+        }
+    }
+}
